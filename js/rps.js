e8f47ba6d85c354aebe0074f22c70f5ebea63c41
Musab <musab.k96@gmail.com>
2022-09-07 19:49:28 +0200

    Add layout in index

--- js/rps.js
+++ js/rps.js
@@ -37,26 +37,26 @@ let playerScore = 0;
 let computerScore = 0; 
 
 
-for (let i = 1; i < 6; i++) {
-    console.log("Round " + i)
-    let result = playRound(getUserChoice(),getComputerChoice());
-    if (result == 1) {
-    } else if (result == 2) {
-        computerScore += 1;
-    } else {
-        playerScore += 1;
-    }
-    console.log("Scoreboard: ");
-    console.log("Player: " + playerScore + "     " + "Computer: " + computerScore);
-    if (i == 5) {
-        if(computerScore > playerScore) {
-            console.log("Computer WON the Game!");
-        } else if (computerScore < playerScore) {
-            console.log("You WON the Game!");
-        } else {
-            console.log("Game is Tied the Game!");
-        }
-    }
+// for (let i = 1; i < 6; i++) {
+//     console.log("Round " + i)
+//     let result = playRound(getUserChoice(),getComputerChoice());
+//     if (result == 1) {
+//     } else if (result == 2) {
+//         computerScore += 1;
+//     } else {
+//         playerScore += 1;
+//     }
+//     console.log("Scoreboard: ");
+//     console.log("Player: " + playerScore + "     " + "Computer: " + computerScore);
+//     if (i == 5) {
+//         if(computerScore > playerScore) {
+//             console.log("Computer WON the Game!");
+//         } else if (computerScore < playerScore) {
+//             console.log("You WON the Game!");
+//         } else {
+//             console.log("Game is Tied the Game!");
+//         }
+//     }
 
-}
+// }
 
