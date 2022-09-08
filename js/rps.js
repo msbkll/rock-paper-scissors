d1ec41d884b8f8ba96e8b24b5d162c84387b49c0
Musab <musab.k96@gmail.com>
2022-09-08 17:40:59 +0200

    Change merge testjs with rpsjs file

--- js/rps.js
+++ js/rps.js
@@ -9,54 +9,43 @@ function getComputerChoice () {
     };
 }
 
-function getUserChoice() {
-    let userChoice = prompt("Choose Rock, Paper or Scissor. :").toLowerCase();
-    return userChoice;
-}
-
 function playRound (playerSelection, computerSelection) {
-    
-
     if (playerSelection == computerSelection) {
         console.log("You chose " + playerSelection + "-----" + "Computer chose " + computerSelection);
         console.log("its a TIE!");
         return 1
-    } else if ((playerSelection == "rock" && computerSelection == "paper") || playerSelection == "paper" && computerSelection == "scissor") {
+    } else if ((playerSelection === "rock" && computerSelection === "paper") 
+    || (playerSelection === "paper" && computerSelection === "scissor") 
+    || (playerSelection === "scissor" && computerSelection === "rock")) { // ++ the computer score when Computer wins
         console.log("You chose " + playerSelection + "-----" + "Computer chose " + computerSelection);
         console.log("You Lose!")
+        
+        computerScore = document.getElementById("computer-score");
+        computerScore.textContent++;
         return 2
-    } else {
+    } else { // + the player score when player wins
         console.log("You chose " + playerSelection + "-----" + "Computer chose " + computerSelection);
         console.log("You WON! CONGRATULATIONS!!!")
+        
+        playerScore = document.getElementById("player-score");
+        playerScore.textContent++;
         return 3
     }
 
 }
 
-let playerScore = 0;
-let computerScore = 0; 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
 
-
-for (let i = 1; i < 6; i++) {
-    console.log("Round " + i)
-    let result = playRound(getUserChoice(),getComputerChoice());
-    if (result == 1) {
-    } else if (result == 2) {
-        computerScore += 1;
-    } else {
-        playerScore += 1;
-    }
-    console.log("Scoreboard: ");
-    console.log("Player: " + playerScore + "     " + "Computer: " + computerScore);
-    if (i == 5) {
-        if(computerScore > playerScore) {
-            console.log("Computer WON the Game!");
-        } else if (computerScore < playerScore) {
-            console.log("You WON the Game!");
-        } else {
-            console.log("Game is Tied the Game!");
-        }
-    }
-
-}
+rock.addEventListener("click", (e) => {
+    console.log(e.target.id)
+    playRound(e.target.id,getComputerChoice());
+});
+paper.addEventListener("click", (e) => {
+    playRound(e.target.id,getComputerChoice());
+});
+scissor.addEventListener("click", (e) => {
+    playRound(e.target.id,getComputerChoice());
+});
 
